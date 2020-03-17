6539fdde84ee45d6c77bf558b265c8ca486e4b68
Tim Adams <adams.timothy.d@gmail.com>
2020-03-16 23:56:25 -0700

    Recursion is harder than it looks, thinking about your base case and your recursive case pays dividends.  Using the 'children' property on a node only selects element type children (it's more specific than 'childNodes')

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,34 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // You should use document.body, element.childNodes and 
+  //element.classList
+  //children property is similar to childNodes, BUT ONLY CONTAINS
+  //element (type 1) nodes!
+
+  //array to store any node elements with className
+  var matches = [];
+
+  var search = function(node) {
+
+    //search each element node's classList for className
+    if (node.classList.contains(className)) {
+      //push to matches array if it does
+      matches.push(node);
+    }
+
+    //recursively search children if the node has children
+    if (node.hasChildNodes()) {
+      for (let i = 0; i < node.children.length; i++) {
+        search(node.children[i]);
+      }
+    }
+  };
+
+  //start search at document.body node
+  search(document.body);
+
+  return matches;
+
 };
