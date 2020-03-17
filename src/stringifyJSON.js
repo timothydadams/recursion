d7e91ab5c7101121a007d1f54bf5457807424b70
Tim Adams <adams.timothy.d@gmail.com>
2020-03-16 19:21:52 -0700

    1st attempt at stringifyJSON, some room for improvement.  Can't handle escape characters.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,62 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  console.log(JSON.stringify(obj));
+  
+  function addToString(obj, string) {
+  	//handle some edge cases
+  	if (typeof obj === `number` || obj === null || typeof obj === `boolean`) {
+  		return `${obj}`;
+  	}
+
+  	//handle strings
+  	if (typeof obj === `string`) {
+  		return `"${obj}"`;
+  	}
+
+  	//handle arrays
+  	if (Array.isArray(obj)) {
+  	  if (obj.length === 0) return `[]`;
+
+  	  string = `[`;
+
+  	  for (var i = 0; i < obj.length; i++) {
+  	    string += addToString(obj[i], string);
+  		if (obj.length > 1 && i < obj.length - 1) {
+  		  string += `,`;
+  		}
+  	  }
+  	  return string + `]`;
+  	}
+
+  	//handle objects
+  	if (obj instanceof Object) {
+
+  	  if (Object.entries(obj).length === 0) {
+  	  	return `{}`;
+  	  }
+
+  	  var keys = Object.keys(obj);
+
+  	  string = `{`;
+  	  for (var j = 0; j < keys.length; j++) {
+  	  	if (typeof obj[keys[j]] === undefined || obj[keys[j]] instanceof Function) {
+  	  		string += `''`;
+  	  	} else {
+  	  	  string += `"${keys[j]}"`;
+  	  	  string += `:`;
+  	  	  string += addToString(obj[keys[j]], string);
+  	  	}
+  	  	if (keys.length > 1 && j < keys.length - 1) {
+  	      string += `,`;
+  	  	}
+  	  }
+  	  return string + `}`;
+  	}
+  }
+ 
+   console.log(addToString(obj, ``));
+   return addToString(obj, ``);
+
 };
